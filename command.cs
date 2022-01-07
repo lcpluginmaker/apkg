68de93a0ed34e3c28e2b7ad5b417a3cd3a00a61f
alexcoder04 <alexcoder04@protonmail.com>
2022-01-07 13:58:38 +0100

    nice output and code refactoring; add compile from git option

--- command.cs
+++ command.cs
@@ -44,66 +44,39 @@ namespace LeoConsole_apkg
     // ------- APKG COMMANDS -------
     private void apkg_do_get() {
       if (_InputProperties.Length < 3){
-        Console.WriteLine("you need to provide a package name or location");
+        Console.WriteLine(" => error: you need to provide a package name or location");
         return;
       }
       if (_InputProperties[2].StartsWith("https://")){
         string url = _InputProperties[2];
         if (url.EndsWith(".git")) {
-          Console.WriteLine("installing git repositories is not supported yet");
+          get_git(url);
           return;
         }
-        if (!url.EndsWith(".dll")) {
-          Console.WriteLine("only downoading .dll files is supported");
+        if (url.EndsWith(".dll")) {
+          get_http(url);
           return;
         }
-        string downloadPath = Path.Join("data", "tmp", "plugins", Path.GetFileName(url));
-        if (!downloadFile(url, downloadPath)) {
-          Console.WriteLine("error downloading");
-          return;
-        }
-        Console.WriteLine("installing downloaded package...");
-        try {
-          File.Copy(downloadPath, Path.Join("data", "plugins", Path.GetFileName(downloadPath)), true);
-        } catch (Exception e) {
-          Console.WriteLine("error copying: " + e.Message);
-          return;
-        }
-        Console.WriteLine(url + " was installed. restart LeoConsole to enable it.");
+        Console.WriteLine(" => error: only downoading .dll files or git repositories is supported");
         return;
       }
       if (_InputProperties[2].StartsWith("file://")){
         string folder = _InputProperties[2].Substring(7, _InputProperties[2].Length-7);
-        Console.WriteLine("installing " + folder + "...");
-        if (!Directory.Exists(folder)) {
-          Console.WriteLine(folder + "does not exist");
-          return;
-        }
-        if (!compileFolder(folder)) {
-          return;
-        }
-        Console.WriteLine("installing compiled plugin...");
-        foreach (string filename in Directory.GetFiles(Path.Join(folder, "bin", "Debug", "net6.0"))){
-          if (filename.EndsWith(".dll")){
-            Console.WriteLine("copying " + filename + " to " + Path.Join("data", "plugins", Path.GetFileName(filename)) + "...");
-            try {
-              File.Copy(filename, Path.Join("data", "plugins", Path.GetFileName(filename)), true);
-            } catch (Exception e) {
-              Console.WriteLine("Error copying " + filename + ": " + e.Message);
-              return;
-            }
-          }
-        }
-        Console.WriteLine(Path.GetFileName(folder) + " was installed. restart LeoConsole to load it");
+        get_file(folder);
         return;
       }
-      Console.WriteLine("downloading normal plugins is not supported yet");
-      Console.WriteLine("please use pkg get " + InputProperties[2]);
+      Console.WriteLine(" => downloading normal plugins is not supported yet");
+      Console.WriteLine("   -> please use pkg get " + InputProperties[2]);
     }
 
     private void apkg_do_list_installed() {
-      foreach (string filename in Directory.GetFiles(Path.Join("data", "plugins"))){
-        Console.WriteLine(filename);
+      Console.WriteLine("=> your installed plugin files:");
+      try {
+        foreach (string filename in Directory.GetFiles(Path.Join("data", "plugins"))){
+          Console.WriteLine("   -> " + filename);
+        }
+      } catch (Exception e) {
+        Console.WriteLine("   -> error: " + e.Message);
       }
     }
 
@@ -129,40 +102,130 @@ namespace LeoConsole_apkg
     }
 
     // ------- HELPER FUNCTIONS -------
+    // DIFFERENT TYPES OF INSTALLATION
+    // from local folder
+    private void get_file(string folder) {
+      Console.WriteLine(" => installing from local folder...");
+      if (!Directory.Exists(folder)) {
+        Console.WriteLine("   -> error: " + folder + "does not exist");
+        return;
+      }
+      if (!compileFolder(folder)) {
+        return;
+      }
+      if (!install_dlls(folder)) {
+        return;
+      }
+      Console.WriteLine(" => " + folder + " was installed. restart LeoConsole to load it");
+    }
+
+    // compiled dll from http
+    private void get_http(string url) {
+      Console.WriteLine(" => installing compiled dll file");
+      string downloadPath = Path.Join("data", "tmp", "plugins", Path.GetFileName(url));
+      if (!downloadFile(url, downloadPath)) {
+        return;
+      }
+      Console.WriteLine(" => installing downloaded dll...");
+      try {
+        File.Copy(downloadPath, Path.Join("data", "plugins", Path.GetFileName(downloadPath)), true);
+      } catch (Exception e) {
+        Console.WriteLine("   -> error installing: " + e.Message);
+        return;
+      }
+      Console.WriteLine(" => " + url + " was installed. restart LeoConsole to enable it.");
+    }
+
+    // compiling from git repository
+    private void get_git(string url) {
+      Console.WriteLine(" => installing from git repository");
+      string name = Path.GetFileName(url).Split(".")[0];
+      if (!gitClone(url, Path.Join("data", "tmp", "plugins"), name)) {
+        return;
+      }
+      if (!compileFolder(Path.Join("data", "tmp", "plugins", name))) {
+        return;
+      }
+      if (!install_dlls(Path.Join("data", "tmp", "plugins", name))) {
+        return;
+      }
+      Console.WriteLine(" => " + url + " was installed. restart LeoConsole to load it");
+      return;
+    }
+
+    // MORE LOW-LEVEL HELPER FUNCTIONS
+    // download a file to given location
     private bool downloadFile(string url, string location) {
-      Console.WriteLine("downloading " + url + " to " + location + "...");
+      Console.WriteLine(" => downloading " + url + " to " + location + "...");
       try {
         WebClient webClient = new WebClient();
         webClient.DownloadFile(url, location);
       } catch (Exception e) {
-        Console.WriteLine("error downloading: " + e.Message);
+        Console.WriteLine("   -> error downloading: " + e.Message);
         return false;
       }
       return true;
     }
 
+    // install all dlls from bin subfolder of a repository
+    private bool install_dlls(string from_folder) {
+      Console.WriteLine(" => installing dlls from " + from_folder + "...");
+      try {
+        foreach (string filename in Directory.GetFiles(Path.Join(from_folder, "bin", "Debug", "net6.0"))){
+          if (filename.EndsWith(".dll")){
+            Console.WriteLine("   -> copying " + filename + " to " + Path.Join("data", "plugins", Path.GetFileName(filename)) + "...");
+            File.Copy(filename, Path.Join("data", "plugins", Path.GetFileName(filename)), true);
+          }
+        }
+      } catch (Exception e) {
+        Console.WriteLine("   -> error installing: " + e.Message);
+        return false;
+      }
+      return true;
+    }
+
+    // clone a repository to given location
+    private bool gitClone(string url, string parent, string folder) {
+      Console.WriteLine(" => cloning " + url + " to " + parent + "/" + folder + "...");
+      if (!runProcess("git", "clone " + url + " " + folder, parent)) {
+        return false;
+      }
+      Console.WriteLine("   -> cloned repo successfully");
+      return true;
+    }
+
+    // compile a repository with dotnet
     private bool compileFolder(string folder) {
-      Console.WriteLine("compiling " + folder + " with dotnet...");
+      Console.WriteLine(" => compiling " + folder + " with dotnet...");
+      if (!runProcess("dotnet", "build", folder)) {
+        return false;
+      }
+      Console.WriteLine("   -> compiled " + folder + " successfully");
+      return true;
+    }
+
+    // run a process with parameters and wait for it to finish
+    private bool runProcess(string name, string args, string pwd) {
       try {
         Process p = new Process();
-        p.StartInfo.FileName = "dotnet";
-        p.StartInfo.Arguments = "build";
-        p.StartInfo.WorkingDirectory = folder;
+        p.StartInfo.FileName = name;
+        p.StartInfo.Arguments = args;
+        p.StartInfo.WorkingDirectory = pwd;
         p.Start();
         p.WaitForExit();
-        if (p.ExitCode != 0){
-          Console.WriteLine("failed compiling: dotnet failed");
+        if (p.ExitCode != 0) {
+          Console.WriteLine("   -> " + name + " returned an error");
           return false;
         }
       } catch (Exception e) {
-        Console.WriteLine("Error compiling: " + e.Message);
+        Console.WriteLine("   -> error running " + name + ": " + e.Message);
         return false;
       }
-      Console.WriteLine("compiled successfully");
       return true;
     }
   }
 }
 
+// keep this, this is the most important thing
 // vim: tabstop=2 softtabstop=2 shiftwidth=2 expandtab
 
